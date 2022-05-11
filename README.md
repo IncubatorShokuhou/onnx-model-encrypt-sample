540111aefb9f55a60fea32662f173fb69279c1cf
IncubatorShokuhou <1057304556@qq.com>
2022-05-11 11:52:48 +0800

    translate into zh_CN

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
 # onnx-model-encrypt-sample
-ONNXモデルを[pyca/cryptography](https://github.com/pyca/cryptography)を用いて暗号化/復号化するサンプルです。<br>
-※Pythonスクリプトで実施する都合上、気休め程度です。<br>　セキュリティを考慮した運用を考える場合、復号化するプログラムはC++等で実行ファイルを作成することをおすすめします。
-
+使用[pyca/cryptography](https://github.com/pyca/cryptography)对ONNX模型进行加密/解密的样例。<br>
+※为了方便起见，使用了python脚本执行。<br>
+考虑到安全性，我们建议解密的程序使用C++等创建可执行文件。<br>
 # Requirement 
 * cryptography 36.0.2 or later
 * onnxruntime 1.10.0 or later
 
 # Usage
-サンプルの実行方法は以下です。<br>
-各プログラムを実行後に任意のパスワードを入力してください。<br>
-暗号化実施時には「resnet50-v1-12.dat」(ONNXモデルを暗号化したもの)を出力し<br>
-復号化実施時には「resnet50-v1-12.dat」を読み込みます。
+以下是如何运行的示例。运行每个程序后输入任意密码。<br>
+实施加密时输出「resnet50-v1-12.dat」(onnx模型加密)<br>
+在执行解码时，读取「resnet50-v1-12.dat」。<br>
 ```bash
 python encrypt_sample.py 
 Password:
@@ -28,4 +27,3 @@ Password:
  
 # License 
 onnx-model-encrypt-sample is under [Apache-2.0 License](LICENSE).
-
--- decrypt_sample.py
+++ decrypt_sample.py
@@ -8,12 +8,12 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 
 def decrypt(
-    encryp_data,
+    encrypt_data,
     password_text,
     salt=b'salt',
     stretching_iterations=390000,
 ):
-    # ソルト付与・ストレッチング実施用インスタンス生成
+    # 用于盐值和拉伸迭代次数，进行实例化
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
         length=32,
@@ -21,17 +21,17 @@ def decrypt(
         iterations=stretching_iterations,
     )
 
-    # キー生成
+    # 生成密钥
     password = password_text.encode("utf-8")
     key = base64.urlsafe_b64encode(kdf.derive(password))
 
-    # Fernetオブジェクトを指定キーで生成
+    # 使用指定密钥生成 Fernet对象
     fernet = Fernet(key)
 
-    # 復号化
+    # 加密
     decrypt_data = None
     try:
-        decrypt_data = fernet.decrypt(encryp_data)
+        decrypt_data = fernet.decrypt(encrypt_data)
     except InvalidToken:
         print('Invalid Password')
 
@@ -39,17 +39,17 @@ def decrypt(
 
 
 def main():
-    # 暗号化したデータの読み込み
+    # 读取加密数据
     with open('resnet50-v1-12.dat', 'rb') as f:
         encryp_data = f.read()
 
-    # パスワード入力
+    # 输入密码
     password_text = input('Password:')
 
-    # 復号化
+    # 解密
     decrypt_data = decrypt(encryp_data, password_text)
 
-    # 復号化したONNXモデルの読み込み確認
+    # 确认读取解密的 ONNX 模型
     if decrypt_data is not None:
         onnx_session = onnxruntime.InferenceSession(
             decrypt_data,
